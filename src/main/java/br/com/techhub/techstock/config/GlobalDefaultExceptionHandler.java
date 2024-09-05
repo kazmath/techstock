ff5ebf54a795f6a1150eedb545667b99fe0f85f9
Matheus H <66137568+kazmath@users.noreply.github.com>
2024-09-04 23:33:10 -0300

    Tratar erros

---
+++ src/main/java/br/com/techhub/techstock/config/GlobalDefaultExceptionHandler.java
@@ -0,0 +1,34 @@
+package br.com.techhub.techstock.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.techhub.techstock.controller.espelhos.Response;
+
+@ControllerAdvice
+public class GlobalDefaultExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {
+        Exception.class
+    })
+    protected ResponseEntity<Object> handleConflict(
+        RuntimeException exception,
+        WebRequest request
+    ) {
+        Response<Object> response = new Response<>();
+        response.getErrors()
+            .add(
+                String.format(
+                    "%s: %s",
+                    exception.getClass().getSimpleName(),
+                    exception.getMessage()
+                )
+            );
+        exception.printStackTrace();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+}
